3444857f2358b5966693ee32ce35d464d7374147
klugjo <klughertz.j@gmail.com>
2016-02-29 16:15:41 +0800

    Client projet structure setup

---
+++ client/voting-client/src/components/Voting.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default React.createClass({
+    getPair: function() {
+        return this.props.pair || {};
+    },
+    render: function () {
+        return <div className="voting">
+            {
+                this.getPair().map(entry =>
+                    <button key={entry}>
+                        <h1>{entry}</h1>
+                    </button>
+            )}
+        </div>
+    }
+});
